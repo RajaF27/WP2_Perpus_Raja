510ac301bf12f7ad53f539159a0dbfd846714309
RajaF27 <rotibakaraja0@gmail.com>
2021-10-19 13:44:42 +0700

    -

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Halo</h1>";
+        echo "Nama saya Muhammad Raja Farhan Batubara <br>
+        Hobiku bermain basket,nonton film,dan rebahan";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Raja</li>
+        <li>Nama Belakang : Farhan</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Bojonggede </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Samarinda</li>
+    </ul>
+    <h4>Film Favorit</h4>
+    <ul type="square">
+        <li>Naruto</li>
+    </ul>
+</section>
